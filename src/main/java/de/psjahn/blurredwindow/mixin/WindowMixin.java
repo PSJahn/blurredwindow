d30089bfa089949f96151fd3692c9a11b5c8476a
EnderKill98 <no@mail.tld>
2024-05-26 02:22:23 +0200

    Blurred background works (tested on kde). But not in prismlauncher flatpak, yet.

--- src/main/java/de/psjahn/blurredwindow/mixin/WindowMixin.java
+++ src/main/java/de/psjahn/blurredwindow/mixin/WindowMixin.java
@@ -5,7 +5,11 @@ import com.sun.jna.platform.win32.WinDef;
 import de.psjahn.blurredwindow.Dwmapi;
 import net.minecraft.client.util.Window;
 import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWNativeWayland;
 import org.lwjgl.glfw.GLFWNativeWin32;
+import org.lwjgl.glfw.GLFWNativeX11;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -30,6 +34,23 @@ public abstract class WindowMixin {
             Dwmapi.setAcrylicBackground(window);
             Dwmapi.setUseImmersiveDarkMode(window, true);
             Dwmapi.removeBorder(window);
+        }else if(GLFW.glfwGetPlatform() == GLFW.GLFW_PLATFORM_X11) {
+            // Linux (Xorg) - Unless mods like WayGL are used, Minecraft
+            // on Linux will use xorg even on Wayland through xwayland.
+
+            long window = GLFWNativeX11.glfwGetX11Window(getHandle());
+            try {
+                // TODO: Use natively instead of via command
+                Runtime.getRuntime().exec(new String[]{
+                        "xprop",
+                        "-id", "" + window,
+                        "-f", "_KDE_NET_WM_BLUR_BEHIND_REGION",
+                        "32c",
+                        "-set", "_KDE_NET_WM_BLUR_BEHIND_REGION", "1",
+                });
+            }catch (Exception ex) {
+                LoggerFactory.getLogger("blurredwindow").error("Failed to run xprop to enable blur (might be KDE specific)", ex);
+            }
         }
     }
 }
