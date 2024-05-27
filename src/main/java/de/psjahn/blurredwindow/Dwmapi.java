6bbd34120670f1399f2de4c37103185b4759aa23
PSJahn <66626848+PSJahn@users.noreply.github.com>
2024-05-27 16:16:15 +0200

    Removed Debug PrintLn, Code Cleanup

--- src/main/java/de/psjahn/blurredwindow/Dwmapi.java
+++ src/main/java/de/psjahn/blurredwindow/Dwmapi.java
@@ -8,16 +8,21 @@ import com.sun.jna.platform.win32.WinDef.HWND;
 import com.sun.jna.ptr.IntByReference;
 
 public interface Dwmapi extends Library {
-    Dwmapi INSTANCE = Native.loadLibrary("dwmapi", Dwmapi.class);
+    Dwmapi INSTANCE = Native.load("dwmapi", Dwmapi.class);
 
+    int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+    int DWMWA_SYSTEMBACKDROP_TYPE = 38;
+    int DWMWA_BORDER_COLOR = 34;
+
+    @SuppressWarnings("UnusedReturnValue")
     int DwmSetWindowAttribute(HWND hwnd, WinDef.UINT dwAttribute, PointerType pvAttribute, int cbAttribute);
 
     static void setUseImmersiveDarkMode(HWND hwnd, boolean enabled) {
-        System.out.println(INSTANCE.DwmSetWindowAttribute(hwnd, new WinDef.UINT(20), new WinDef.BOOLByReference(new WinDef.BOOL(enabled)), 4));
+        INSTANCE.DwmSetWindowAttribute(hwnd, new WinDef.UINT(DWMWA_USE_IMMERSIVE_DARK_MODE), new WinDef.BOOLByReference(new WinDef.BOOL(enabled)), 4);
     }
 
     static void setAcrylicBackground(HWND hwnd) {
-        INSTANCE.DwmSetWindowAttribute(hwnd, new WinDef.UINT(38), new IntByReference(3), 4);
+        INSTANCE.DwmSetWindowAttribute(hwnd, new WinDef.UINT(DWMWA_SYSTEMBACKDROP_TYPE), new IntByReference(3), 4);
     }
 
     static void removeBorder(HWND hwnd) {
@@ -25,6 +30,6 @@ public interface Dwmapi extends Library {
     }
 
     static void setBorderColor(HWND hwnd, long color) {
-        INSTANCE.DwmSetWindowAttribute(hwnd, new WinDef.UINT(34), new WinDef.UINTByReference(new WinDef.UINT(color)), 4);
+        INSTANCE.DwmSetWindowAttribute(hwnd, new WinDef.UINT(DWMWA_BORDER_COLOR), new WinDef.UINTByReference(new WinDef.UINT(color)), 4);
     }
 }
