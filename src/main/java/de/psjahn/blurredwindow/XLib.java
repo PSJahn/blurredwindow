b719d4562b39ea982fc38243876b0f31a796e31b
PSJahn <66626848+PSJahn@users.noreply.github.com>
2024-05-27 16:28:30 +0200

    Replaced deprecated method call, Code Cleanup

--- src/main/java/de/psjahn/blurredwindow/XLib.java
+++ src/main/java/de/psjahn/blurredwindow/XLib.java
@@ -6,13 +6,18 @@ import com.sun.jna.Native;
 import java.nio.ByteBuffer;
 
 public interface XLib extends Library {
+    XLib INSTANCE = Native.load("libX11", XLib.class);
 
     long XA_CARDINAL_ATOM = 6;
 
-    XLib INSTANCE = Native.loadLibrary("libX11", XLib.class);
-
     long XInternAtom(long displayPtr, String atomName, boolean onlyIfExists);
 
+    @SuppressWarnings("UnusedReturnValue")
     int XChangeProperty(long displayPtr, long window, long/*Atom*/ property, long/*Atom*/ type, int format, int mode, ByteBuffer data, int nElements);
 
+    static void setBlurBehind(long display, long window, boolean enabled) {
+        long property = XLib.INSTANCE.XInternAtom(display, "_KDE_NET_WM_BLUR_BEHIND_REGION", false);
+        byte[] data = enabled ? new byte[]{ 0x00, 0x00, 0x00, 0x01 } : new byte[]{ 0x00, 0x00, 0x00, 0x00 }; // A boolean but in 32 bit and passed as pointer to buffer "data"
+        XLib.INSTANCE.XChangeProperty(display, window, property, XLib.XA_CARDINAL_ATOM, 32, 0, ByteBuffer.wrap(data), 1);
+    }
 }
