35ec919db12231c69f04b86e3345fe572b8612cd
EnderKill98 <no@mail.tld>
2024-06-04 12:17:18 +0200

    Fix disabling BlurBehind not working

--- src/main/java/de/psjahn/blurredwindow/XLib.java
+++ src/main/java/de/psjahn/blurredwindow/XLib.java
@@ -15,9 +15,18 @@ public interface XLib extends Library {
     @SuppressWarnings("UnusedReturnValue")
     int XChangeProperty(long displayPtr, long window, long/*Atom*/ property, long/*Atom*/ type, int format, int mode, ByteBuffer data, int nElements);
 
+    @SuppressWarnings("UnusedReturnValue")
+    int XDeleteProperty(long displayPtr, long window, long/*Atom*/ property);
+
     static void setBlurBehind(long display, long window, boolean enabled) {
-        long property = XLib.INSTANCE.XInternAtom(display, "_KDE_NET_WM_BLUR_BEHIND_REGION", false);
-        byte[] data = enabled ? new byte[]{ 0x00, 0x00, 0x00, 0x01 } : new byte[]{ 0x00, 0x00, 0x00, 0x00 }; // A boolean but in 32 bit and passed as pointer to buffer "data"
-        XLib.INSTANCE.XChangeProperty(display, window, property, XLib.XA_CARDINAL_ATOM, 32, 0, ByteBuffer.wrap(data), 1);
+        long propertyAtom = XLib.INSTANCE.XInternAtom(display, "_KDE_NET_WM_BLUR_BEHIND_REGION", false);
+
+        if(enabled) {
+            // 32 bit value and passed as pointer to buffer "data". Value doesn't seem to matter, but let's just use 0x01
+            byte[] data = new byte[]{0x00, 0x00, 0x00, 0x01};
+            XLib.INSTANCE.XChangeProperty(display, window, propertyAtom, XLib.XA_CARDINAL_ATOM, 32, 0, ByteBuffer.wrap(data), 1);
+        }else {
+            XLib.INSTANCE.XDeleteProperty(display, window, propertyAtom);
+        }
     }
 }
